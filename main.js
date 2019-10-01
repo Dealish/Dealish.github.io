22467ad18ab2da91c3d5fec73d8a62f64f123eb0
William Bolton <williambolton@MacBook-Pro.local>
2019-10-01 19:14:25 -0400

    HEY

---
+++ main.js
@@ -0,0 +1,17 @@
+var a = 1;
+
+function toContact(){
+	window.location.assign("https://quinm2.github.io/myFirstWebsite/contact.html");
+}
+
+function toABout(){
+	self.location = "https://quinm2.github.io/myFirstWebsite/about.html";
+}
+	
+function toIndex(){
+	self.location = "https://quinm2.github.io/myFirstWebsite/index.html";
+}
+	
+function toHome(){
+	self.location = "https://quinm2.github.io/myFirstWebsite/home.html";
+}
