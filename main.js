777a59e441387e2924068178cb59a6247ae4b848
QUIN <qspic096@uottawa.ca>
2019-10-01 19:25:29 -0500

    Changed redirect so it stays on the Dealish website rather than going back to the old one

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 var a = 1;
 
 function toContact(){
-	window.location.href = "https://alcauda.github.io/gogro.github.io/contact.html";
+	window.location.href = "https://dealish.github.io/contact.html";
 }
 
 function toAbout(){
-	window.location.href = "https://alcauda.github.io/gogro.github.io/about.html";
+	window.location.href = "https://dealish.github.io/about.html";
 }
 	
 function toIndex(){
-	window.location.href = "https://alcauda.github.io/gogro.github.io/index.html";
+	window.location.href = "https://dealish.github.io/index.html";
 }
 	
 function toHome(){
-	window.location.href = "https://alcauda.github.io/gogro.github.io/home.html";
+	window.location.href = "https://dealish.github.io/home.html";
 }
