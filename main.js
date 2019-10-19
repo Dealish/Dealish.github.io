0b0e38048472ff78937629b182d2ce5fddaed9d9
Billy B <bbolt049@uottawa.ca>
2019-10-19 18:01:12 -0400

    working on scroll footer

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ function toContact(){
 function toAbout(){
 	window.location.href = "https://dealish.github.io/about.html";
 }
-	
+
 function toIndex(){
 	window.location.href = "https://dealish.github.io/index.html";
 }
-	
+
 function toHome(){
 	window.location.href = "https://dealish.github.io/home.html";
 }
