14b0be4e77b6c48782ed7d2780cafa4fbb0d2a8e
Billy B <bbolt049@uottawa.ca>
2019-11-04 21:10:09 -0500

    fixed timer

--- demo/js/main.js
+++ demo/js/main.js
@@ -314,7 +314,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("11 November 2019 12:00:00 GMT+01:00");			
+		var endTime = new Date("11 November 2019 12:00:00 GMT+01:00");
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
